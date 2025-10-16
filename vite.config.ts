1dba12c412f78c8437a6a56ecde849af70261a90
Reignear <reignearm@gmail.com>
2025-10-16 21:05:07 +0800

    Modified

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/debut-invitation",
+  base: process.env.VITE_BASE_URL || "/debut-invitation",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
