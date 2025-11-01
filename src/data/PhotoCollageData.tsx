670c0f7041d5eb631fea2632e2532ccb97c344e2
Reignear <reignearm@gmail.com>
2025-11-01 17:28:58 +0800

    Modified

--- src/data/PhotoCollageData.tsx
+++ src/data/PhotoCollageData.tsx
@@ -1,19 +1,14 @@
-import white1 from "/pictorial/white/pic1.jpg";
-import white2 from "/pictorial/white/pic2.jpg";
-import white3 from "/pictorial/white/pic3.jpg";
-import white4 from "/pictorial/white/pic4.jpg";
-
 export const themes = {
   black: [
     {
       id: 1,
       url: "/pictorial/black/image2.jpg",
-      span: { default: 2, sm: 1, md: 2, lg: 2 },
+      span: { default: 2, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 2,
       url: "/pictorial/black/image6.jpg",
-      span: { default: 1, sm: 2, md: 1, lg: 1 },
+      span: { default: 1, sm: 1, md: 1, lg: 1 },
     },
     {
       id: 3,
@@ -23,84 +18,84 @@ export const themes = {
     {
       id: 4,
       url: "/pictorial/black/image7.jpg",
-      span: { default: 2, sm: 1, md: 1, lg: 1 },
+      span: { default: 2, sm: 2, md: 1, lg: 1 },
     },
     {
       id: 5,
       url: "/pictorial/black/image5.jpg",
-      span: { default: 1, sm: 2, md: 2, lg: 2 },
+      span: { default: 1, sm: 1, md: 2, lg: 2 },
     },
     {
       id: 6,
       url: "/pictorial/black/image1.jpg",
-      span: { default: 1, sm: 1, md: 1, lg: 1 },
+      span: { default: 1, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 7,
       url: "/pictorial/black/image11.jpg",
-      span: { default: 2, sm: 1, md: 1, lg: 1 },
+      span: { default: 2, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 8,
-      url: "/pictorial/black/image8.jpg",
-      span: { default: 1, sm: 1, md: 2, lg: 2 },
+      url: "/pictorial/black/image12.jpg",
+      span: { default: 1, sm: 1, md: 1, lg: 1 },
     },
     {
       id: 9,
       url: "/pictorial/black/image10.jpg",
-      span: { default: 1, sm: 1, md: 1, lg: 1 },
+      span: { default: 1, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 10,
       url: "/pictorial/black/image9.jpg",
-      span: { default: 2, sm: 1, md: 1, lg: 1 },
+      span: { default: 2, sm: 2, md: 2, lg: 2 },
     },
     {
-      id: 11,
+      id: 31,
       url: "/pictorial/black/image4.jpg",
       span: { default: 1, sm: 1, md: 1, lg: 1 },
     },
     {
-      id: 12,
-      url: "/pictorial/black/image12.jpg",
-      span: { default: 1, sm: 1, md: 1, lg: 1 },
+      id: 32,
+      url: "/pictorial/black/image8.jpg",
+      span: { default: 1, sm: 2, md: 2, lg: 2 },
     },
   ],
   red: [
     {
       id: 11,
       url: "/pictorial/red/image1.jpg",
-      span: { default: 2, sm: 1, md: 1, lg: 1 },
+      span: { default: 2, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 12,
       url: "/pictorial/red/image2.jpg",
-      span: { default: 1, sm: 1, md: 1, lg: 1 },
+      span: { default: 1, sm: 1, md: 2, lg: 2 },
     },
     {
       id: 13,
       url: "/pictorial/red/image3.jpg",
-      span: { default: 1, sm: 1, md: 2, lg: 1 },
+      span: { default: 1, sm: 1, md: 1, lg: 1 },
     },
     {
       id: 14,
       url: "/pictorial/red/image7.jpg",
-      span: { default: 2, sm: 1, md: 2, lg: 1 },
+      span: { default: 2, sm: 2, md: 1, lg: 1 },
     },
     {
       id: 15,
       url: "/pictorial/red/image9.jpg",
-      span: { default: 1, sm: 1, md: 1, lg: 2 },
+      span: { default: 1, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 16,
       url: "/pictorial/red/image6.jpg",
-      span: { default: 1, sm: 1, md: 1, lg: 2 },
+      span: { default: 1, sm: 1, md: 2, lg: 2 },
     },
     {
       id: 17,
       url: "/pictorial/red/image4.jpg",
-      span: { default: 2, sm: 1, md: 1, lg: 2 },
+      span: { default: 2, sm: 1, md: 1, lg: 1 },
     },
     {
       id: 18,
@@ -115,59 +110,59 @@ export const themes = {
     {
       id: 20,
       url: "/pictorial/red/image10.jpg",
-      span: { default: 2, sm: 1, md: 1, lg: 2 },
+      span: { default: 2, sm: 2, md: 1, lg: 2 },
     },
   ],
   white: [
     {
       id: 21,
-      url: white1,
-      span: { default: 1, sm: 1, md: 2, lg: 1 },
+      url: "/pictorial/white/image2.jpg",
+      span: { default: 2, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 22,
-      url: white3,
-      span: { default: 1, sm: 1, md: 1, lg: 2 },
+      url: "/pictorial/white/image1.jpg",
+      span: { default: 1, sm: 1, md: 2, lg: 2 },
     },
     {
       id: 23,
-      url: white2,
-      span: { default: 1, sm: 1, md: 1, lg: 2 },
+      url: "/pictorial/white/image3.jpg",
+      span: { default: 1, sm: 1, md: 1, lg: 1 },
     },
     {
       id: 24,
-      url: white4,
+      url: "/pictorial/white/image5.jpg",
+      span: { default: 2, sm: 2, md: 1, lg: 1 },
+    },
+    {
+      id: 25,
+      url: "/pictorial/white/image4.jpg",
+      span: { default: 1, sm: 2, md: 2, lg: 2 },
+    },
+    {
+      id: 26,
+      url: "/pictorial/white/image6.jpg",
+      span: { default: 1, sm: 1, md: 2, lg: 2 },
+    },
+    {
+      id: 27,
+      url: "/pictorial/white/image7.jpg",
+      span: { default: 2, sm: 1, md: 1, lg: 1 },
+    },
+    {
+      id: 28,
+      url: "/pictorial/white/image8.jpg",
       span: { default: 1, sm: 1, md: 1, lg: 1 },
     },
-    // {
-    //   id: 25,
-    //   url: "https://images.unsplash.com/photo-1553356084-58ef4a67b2a7?w=600&h=800",
-    //   span: { default: 1, sm: 1, md: 2, lg: 1 },
-    // },
-    // {
-    //   id: 26,
-    //   url: "https://images.unsplash.com/photo-1567359781514-3b964e2b04d6?w=600&h=600",
-    //   span: { default: 1, sm: 1, md: 1, lg: 2 },
-    // },
-    // {
-    //   id: 27,
-    //   url: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809?w=600&h=900",
-    //   span: { default: 1, sm: 1, md: 1, lg: 2 },
-    // },
-    // {
-    //   id: 28,
-    //   url: "https://images.unsplash.com/photo-1554034483-04fda0d3507b?w=600&h=550",
-    //   span: { default: 2, sm: 2, md: 2, lg: 1 },
-    // },
-    // {
-    //   id: 29,
-    //   url: "https://images.unsplash.com/photo-1506792006437-256b665541e2?w=600&h=750",
-    //   span: { default: 1, sm: 1, md: 2, lg: 1 },
-    // },
-    // {
-    //   id: 30,
-    //   url: "https://images.unsplash.com/photo-1518837695005-2083093ee35b?w=600&h=650",
-    //   span: { default: 1, sm: 1, md: 1, lg: 2 },
-    // },
+    {
+      id: 29,
+      url: "/pictorial/white/image9.jpg",
+      span: { default: 1, sm: 1, md: 2, lg: 1 },
+    },
+    {
+      id: 30,
+      url: "/pictorial/white/image10.jpg",
+      span: { default: 2, sm: 2, md: 1, lg: 2 },
+    },
   ],
 };
