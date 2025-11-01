4cadee3be7070b6f7a13953a57a4a5b86c75f566
Reignear <reignearm@gmail.com>
2025-11-01 22:18:30 +0800

    Modified

--- src/data/PhotoCollageData.tsx
+++ src/data/PhotoCollageData.tsx
@@ -2,127 +2,65 @@ export const themes = {
   black: [
     {
       id: 1,
-      url: "https://res.cloudinary.com/dzk3ahsu1/image/upload/v1762001118/image2_r4uf6z.webp",
+      url: "/pictorial/black/image2.webp",
       span: { default: 2, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 2,
-      url: "https://res.cloudinary.com/dzk3ahsu1/image/upload/v1762001120/image6_nsowhy.webp",
+      url: "/pictorial/black/image6.webp",
       span: { default: 1, sm: 1, md: 1, lg: 1 },
     },
     {
       id: 3,
-      url: "https://res.cloudinary.com/dzk3ahsu1/image/upload/v1762001118/image3_mok3bg.webp",
+      url: "/pictorial/black/image3.webp",
       span: { default: 1, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 4,
-      url: "https://res.cloudinary.com/dzk3ahsu1/image/upload/v1762001122/image7_k1j2sv.webp",
+      url: "/pictorial/black/image7.webp",
       span: { default: 2, sm: 2, md: 1, lg: 1 },
     },
     {
       id: 5,
-      url: "https://res.cloudinary.com/dzk3ahsu1/image/upload/v1762001122/image5_izbnqy.webp",
+      url: "/pictorial/black/image5.webp",
       span: { default: 1, sm: 1, md: 2, lg: 2 },
     },
     {
       id: 6,
-      url: "https://res.cloudinary.com/dzk3ahsu1/image/upload/v1762001122/image1_ubotnh.webp",
+      url: "/pictorial/black/image1.webp",
       span: { default: 1, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 7,
-      url: "https://res.cloudinary.com/dzk3ahsu1/image/upload/v1762001123/image11_h0szbz.webp",
+      url: "/pictorial/black/image11.webp",
       span: { default: 2, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 8,
-      url: "https://res.cloudinary.com/dzk3ahsu1/image/upload/v1762001126/image12_dcygom.webp",
+      url: "/pictorial/black/image12.webp",
       span: { default: 1, sm: 1, md: 1, lg: 1 },
     },
     {
       id: 9,
-      url: "https://res.cloudinary.com/dzk3ahsu1/image/upload/v1762001124/image10_ppy1ib.webp",
+      url: "/pictorial/black/image10.webp",
       span: { default: 1, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 10,
-      url: "https://res.cloudinary.com/dzk3ahsu1/image/upload/v1762001124/image9_ifonqh.webp",
+      url: "/pictorial/black/image9.webp",
       span: { default: 2, sm: 2, md: 2, lg: 2 },
     },
     {
       id: 31,
-      url: "https://res.cloudinary.com/dzk3ahsu1/image/upload/v1762001117/image4_f9c9sy.webp",
+      url: "/pictorial/black/image4.webp",
       span: { default: 1, sm: 1, md: 1, lg: 1 },
     },
     {
       id: 32,
-      url: "https://res.cloudinary.com/dzk3ahsu1/image/upload/v1762001121/image8_ettns6.webp",
+      url: "/pictorial/black/image8.webp",
       span: { default: 1, sm: 2, md: 2, lg: 2 },
     },
   ],
-  // black: [
-  //   {
-  //     id: 1,
-  //     url: "/pictorial/black/image2.webp",
-  //     span: { default: 2, sm: 2, md: 2, lg: 2 },
-  //   },
-  //   {
-  //     id: 2,
-  //     url: "/pictorial/black/image6.webp",
-  //     span: { default: 1, sm: 1, md: 1, lg: 1 },
-  //   },
-  //   {
-  //     id: 3,
-  //     url: "/pictorial/black/image3.webp",
-  //     span: { default: 1, sm: 2, md: 2, lg: 2 },
-  //   },
-  //   {
-  //     id: 4,
-  //     url: "/pictorial/black/image7.webp",
-  //     span: { default: 2, sm: 2, md: 1, lg: 1 },
-  //   },
-  //   {
-  //     id: 5,
-  //     url: "/pictorial/black/image5.webp",
-  //     span: { default: 1, sm: 1, md: 2, lg: 2 },
-  //   },
-  //   {
-  //     id: 6,
-  //     url: "/pictorial/black/image1.webp",
-  //     span: { default: 1, sm: 2, md: 2, lg: 2 },
-  //   },
-  //   {
-  //     id: 7,
-  //     url: "/pictorial/black/image11.webp",
-  //     span: { default: 2, sm: 2, md: 2, lg: 2 },
-  //   },
-  //   {
-  //     id: 8,
-  //     url: "/pictorial/black/image12.webp",
-  //     span: { default: 1, sm: 1, md: 1, lg: 1 },
-  //   },
-  //   {
-  //     id: 9,
-  //     url: "/pictorial/black/image10.webp",
-  //     span: { default: 1, sm: 2, md: 2, lg: 2 },
-  //   },
-  //   {
-  //     id: 10,
-  //     url: "/pictorial/black/image9.webp",
-  //     span: { default: 2, sm: 2, md: 2, lg: 2 },
-  //   },
-  //   {
-  //     id: 31,
-  //     url: "/pictorial/black/image4.webp",
-  //     span: { default: 1, sm: 1, md: 1, lg: 1 },
-  //   },
-  //   {
-  //     id: 32,
-  //     url: "/pictorial/black/image8.webp",
-  //     span: { default: 1, sm: 2, md: 2, lg: 2 },
-  //   },
-  // ],
   red: [
     {
       id: 11,
